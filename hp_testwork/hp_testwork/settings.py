26b677a3929999833d4232680faed28e6fa5625b
Zavarin_AN <Zavarin_AN@vologda-city.ru>
2024-07-09 15:03:04 +0300

    fixed docker settings

--- hp_testwork/hp_testwork/settings.py
+++ hp_testwork/hp_testwork/settings.py
@@ -88,12 +88,12 @@ WSGI_APPLICATION = 'hp_testwork.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'PORT': '5432',
     }
 }
 
